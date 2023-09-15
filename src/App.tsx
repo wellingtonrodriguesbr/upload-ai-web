db5f7120b154182e66216d5e9e890e2f1b6977bf
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-09-15 05:59:59 -0300

    feat: initial ui with shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
+import { Separator } from "./components/ui/separator";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-semibold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤️ no NLW da Rocketseat
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
